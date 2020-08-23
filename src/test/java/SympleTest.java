2d8cc81851123bead545c683b53765f194bfbf04
Evgeniy Dashuk <evg.das@gmail.com>
2020-08-23 07:35:28 +0200

    add logger

--- src/test/java/SympleTest.java
+++ src/test/java/SympleTest.java
@@ -3,8 +3,8 @@ import io.qameta.allure.Owner;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.junit.platform.commons.logging.Logger;
-import org.junit.platform.commons.logging.LoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static io.qameta.allure.Allure.parameter;
 import static io.qameta.allure.Allure.step;
@@ -14,6 +14,7 @@ import static org.hamcrest.Matchers.greaterThan;
 @Owner("evgdas")
 @Feature("Работа с Jenkins")
 public class SympleTest {
+    static Logger logger = LoggerFactory.getLogger(SympleTest.class);
     @Test
     @DisplayName("Проверка умножения")
     @Tag("multiple_test")
@@ -21,7 +22,9 @@ public class SympleTest {
         String a = System.getProperty("valA", "0");
         String b = System.getProperty("valB", "0");
         int c=Integer.parseInt(a) * Integer.parseInt(b);
-        System.out.println("a= "+a+" b= "+b+" c= "+c);
+        logger.info("valA= "+a);
+        logger.info("valB"+b);
+        logger.info("result= "+c);
         parameter("Множитель а", a);
         parameter("Множитель b", b);
         step("Проверка результатов операции", () -> {

8eadd88065f4422a44a39d514d96c43a712fdbce
Evgeniy Dashuk <evg.das@gmail.com>
2020-08-27 07:27:58 +0200

    add plus test and threadRun

--- README.md
+++ README.md
@@ -8,6 +8,6 @@
 Проверка, что результат умножения больше нуля
 
 запуск теста:
-gradle jenkins_test -DvalA=10 -DvalB=10
+gradle jenkins_test -DvalA=10 -DvalB=10 -Dweb-threads=2
 
   
--- src/test/java/arifmetic/ArifmeticTest.java
+++ src/test/java/arifmetic/ArifmeticTest.java
@@ -1,8 +1,11 @@
+package arifmetic;
+
 import io.qameta.allure.Feature;
 import io.qameta.allure.Owner;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,8 +16,9 @@ import static org.hamcrest.Matchers.greaterThan;
 
 @Owner("evgdas")
 @Feature("Работа с Jenkins")
-public class SympleTest {
-    static Logger logger = LoggerFactory.getLogger(SympleTest.class);
+@TestInstance(TestInstance.Lifecycle.PER_METHOD)
+public class ArifmeticTest {
+    static Logger logger = LoggerFactory.getLogger(ArifmeticTest.class);
     @Test
     @DisplayName("Проверка умножения")
     @Tag("multiple_test")
@@ -23,12 +27,31 @@ public class SympleTest {
         String b = System.getProperty("valB", "0");
         int c=Integer.parseInt(a) * Integer.parseInt(b);
         logger.info("valA= "+a);
-        logger.info("valB"+b);
+        logger.info("valB= "+b);
         logger.info("result= "+c);
         parameter("Множитель а", a);
         parameter("Множитель b", b);
         step("Проверка результатов операции", () -> {
+           parameter("Результат c", c);
            assertThat(c,greaterThan(0));
+        });
+    }
+
+    @Test
+    @DisplayName("Проверка сложения")
+    @Tag("plus_test")
+    public void isRightPlus() {
+        String a = System.getProperty("valA", "0");
+        String b = System.getProperty("valB", "0");
+        int c=Integer.parseInt(a) + Integer.parseInt(b);
+        logger.info("valA= "+a);
+        logger.info("valB= "+b);
+        logger.info("result= "+c);
+        parameter("Слагаемое а", a);
+        parameter("Слагаемое b", b);
+        step("Проверка результатов операции", () -> {
+            parameter("Результат c", c);
+            assertThat(c,greaterThan(0));
         });
     }
 

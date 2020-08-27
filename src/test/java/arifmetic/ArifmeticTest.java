5f7c5c7138c84e9deca0ee2ddf069dbd8258c94f
Evgeniy Dashuk <56819251+evgdas@users.noreply.github.com>
2020-08-27 14:23:05 +0200

    Update ArifmeticTest.java
    add @Execution(ExecutionMode.CONCURRENT)

--- src/test/java/arifmetic/ArifmeticTest.java
+++ src/test/java/arifmetic/ArifmeticTest.java
@@ -17,6 +17,7 @@ import static org.hamcrest.Matchers.greaterThan;
 @Owner("evgdas")
 @Feature("Работа с Jenkins")
 @TestInstance(TestInstance.Lifecycle.PER_METHOD)
+@Execution(ExecutionMode.CONCURRENT)
 public class ArifmeticTest {
     static Logger logger = LoggerFactory.getLogger(ArifmeticTest.class);
     @Test
